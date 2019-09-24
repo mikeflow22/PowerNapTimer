7dcbc7d41d3422f95e240beac05494401518ecdc
Michael R.F <mrflowers22@uwalumni.com>
2019-09-24 10:20:53 -0600

    implemented the timerDelegate protocol

--- PowerNapTimer/Model Controllers/TimerController.swift
+++ PowerNapTimer/Model Controllers/TimerController.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+
+protocol TimerDelegate: class {
+    func timerSecondTick()
+    func timerCompleted()
+    func timerStopped()
+}
+
+class TimerController {
+    //source of truth
+    var timer: Timer?
+    var timeRemaining: TimeInterval?
+    weak var delegate: TimerDelegate?
+    
+    func secondTicking(){
+        //want to see how much time is remaining
+        guard let timeLeft = timeRemaining else { return }
+        if timeLeft < 0 {
+            self.timeRemaining = timeLeft - 1
+            //Alert the delegate to do some stuff in this function.
+            delegate?.timerSecondTick()
+        } else {
+            // stop the timer
+            timer?.invalidate()
+            //set time remaining to nil
+            self.timeRemaining = nil
+            //Alert the delegate to do some stuff in this function
+            delegate?.timerCompleted()
+        }
+    }
+    
+    func startTimer(_ time: TimeInterval) {
+        
+    }
+    
+    func stopTimer(){
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
